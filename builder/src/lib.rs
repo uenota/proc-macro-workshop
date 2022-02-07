af3f2a838dbb4dd523e3890436451c4a803397f3
Takaki Ueno <takaki_ueno@caddi.jp>
2022-02-08 02:50:14 +0900

    generate empty builder function

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,15 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
-    proc_macro::TokenStream::new()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let expand = quote! {
+        impl #ident {
+            pub fn builder() {}
+        }
+    };
+    proc_macro::TokenStream::from(expand)
 }
